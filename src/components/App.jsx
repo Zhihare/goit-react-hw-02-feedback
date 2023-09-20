3c42c905bc85ba35b91294e2eb35b77140a90599
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-09-20 22:12:46 +0300

    20.09

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import React from "react";
+import Statistics from "./Statistics/Statistics.jsx";
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Statistics />
     </div>
   );
 };
