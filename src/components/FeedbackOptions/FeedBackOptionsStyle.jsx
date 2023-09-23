551a9f063b2095b84d750f5a2a74ae2d6f9f4ae8
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-09-23 12:59:57 +0300

    Update FeedBackOptionsStyle.jsx

--- src/components/FeedbackOptions/FeedBackOptionsStyle.jsx
+++ src/components/FeedbackOptions/FeedBackOptionsStyle.jsx
@@ -1,29 +1,26 @@
 import styled from "styled-components"
 
 export const FeedbackOptionsStyle = styled.div
-	`
+  `
 display: flex;
 gap: 20px;
 justify-content: center;
 	`
 
 export const FeedBackOptionsSlyleButton = styled.button
-	`
+  `
     display: flex;
     align-items: center;
     gap: 5px;
     padding: 3px;
 
-  align-self: center;
+  
   background-color: #fff;
-  background-image: none;
   background-position: 0 90%;
-  background-repeat: repeat no-repeat;
   background-size: 4px 3px;
-  border-radius: 15px 225px 255px 15px 15px 255px 225px 15px;
   border-style: solid;
   border-width: 2px;
-  box-shadow: rgba(0, 0, 0, .2) 15px 28px 25px -18px;
+  box-shadow: rgba(0, 0, 0, .2) 10px 20px 20px -10px;
   box-sizing: border-box;
   color: #41403e;
   cursor: pointer;
@@ -42,13 +39,12 @@ export const FeedBackOptionsSlyleButton = styled.button
   -webkit-user-select: none;
   touch-action: manipulation;
 	
-
-:hover {
+&:hover {
   box-shadow: rgba(0, 0, 0, .3) 2px 8px 8px -5px;
   transform: translate3d(0, 2px, 0);
 }
 
-:focus {
+&:focus {
   box-shadow: rgba(0, 0, 0, .3) 2px 8px 4px -6px;
 }
 	`	
